6a689cd30541780922424a66e72872e0bc4c21bb
Madeleine Lowry <mlowry55403@gmail.com>
2015-09-11 14:59:33 -0500

    fixed multiple joins, added book

--- solution.sql
+++ solution.sql
@@ -19,20 +19,28 @@ SELECT * FROM employees JOIN favorite_authors ON employees.id = favorite_authors
 
 What are the titles of all the books that are in stock today?
 SELECT is_stocked, title
-  FROM daily_inventory
-  NATURAL JOIN editions
-  NATURAL JOIN books
-  WHERE daily_inventory.is_stocked;
+  FROM daily_inventory t1
+  NATURAL JOIN editions t2
+  JOIN books t3 ON t2.book_id = t3.id
+  WHERE t1.is_stocked;
 
-Insert one of your favorite books into the database. Hint: You’ll want to create data into at least 2 other tables to completely create this book.
+Insert one of your favorite books into the database. 
+Hint: You’ll want to create data into at least 2 other tables 
+to completely create this book.
+  INSERT INTO books 
+  (id, title, author_id, subject_id)
+  VALUES (175, 'The Tiger''s Wife', 7777, 6);
 
+  INSERT INTO authors 
+  (id, last_name, first_name)
+  VALUES (175, 'Obreht', 'Tea');
 
 What authors have books that are not in stock?
 SELECT is_stocked, last_name
-  FROM daily_inventory
-  NATURAL JOIN editions
-  NATURAL JOIN authors
-  WHERE is_stocked = FALSE;
+  FROM daily_inventory t1
+  NATURAL JOIN editions t2
+  JOIN authors t3 ON t2.book_id = t3.id
+  WHERE t1.is_stocked = FALSE;
 
 What is the title of the book that has the most stock?
 SELECT stock
@@ -40,8 +48,7 @@ SELECT stock
     ORDER BY stock DESC;
     (found the highest stock = 89)
 
-  SELECT stock, title
-  FROM stock
-  NATURAL JOIN editions
-  NATURAL JOIN books
-  WHERE stock = 89;
+SELECT t1.isbn, t2.book_id, t3.title
+FROM stock t1 JOIN editions t2 on t1.isbn = t2.isbn
+JOIN books t3 on t2.book_id=t3.id
+WHERE t1.stock = 89;
